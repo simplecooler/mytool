75388aab1043c609e215574ea59dd6e46f974c94
szm <shenzeming@yundun.cn>
2016-01-07 17:33:52 +0800

    add mousewheel.js

---
+++ mousewheel.js
@@ -0,0 +1,15 @@
+//鼠标滚动收缩导航栏
+  $(document).on('mousewheel', function(event) {
+    //console.log(event.deltaX, event.deltaY, event.deltaFactor);
+    if(event.deltaY > 0) {
+      $('.navgation').slideDown('slow');
+    }else {
+      $('.navgation').slideUp('slow');
+    }
+
+    $(window).scroll(function () {
+      if ($(window).scrollTop() == 0) {
+        $('.navgation').slideDown('slow');
+      }
+    });
+  });

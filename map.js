cb1fb2615462488f12e927aea1887abe30ede050
simplecooler <shen.zeming@icloud.com>
2016-02-02 14:00:40 +0800

    second redesign

--- map.js
+++
@@ -1,8 +0,0 @@
-$("#visitChinaMapChart").hide();
-$("#visitWorldMapChart").fadeIn(function () {
-    for(world_count = 1;world_count < 8;world_count++) {
-        visitwordMapOption.series[world_count].markLine.data = [];
-        visitwordMapOption.series[world_count].markPoint.data = [];
-    }
-    worldMapchart.setOption(visitwordMapOption,true);
-});
